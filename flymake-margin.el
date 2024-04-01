062938b9aff3918df4891984d33bcd3ee0107db4
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-04-01 20:34:27 -0300

    fix: checkdoc and package-lint

--- flymake-margin.el
+++ flymake-margin.el
@@ -3,7 +3,7 @@
 ;; Author: Rahul M. Juliato
 ;; Created: March 25, 2024
 ;; Version: 0.1.0
-;; Keywords: flymake, margin, tui
+;; Keywords: languages, maint, tools
 ;; URL: https://github.com/LionyxML/flymake-margin
 ;; Package-Requires: ((emacs "29.1"))
 ;; SPDX-License-Identifier: GPL-3.0-or-later
@@ -66,7 +66,7 @@ Possible values are 'left' and 'right'."
            (modify-all-frames-parameters '((left-fringe . right-margin))))))
 
 (defun flymake-margin-fringe-overlay-spec-advice (bitmap &optional recursed)
-"Advice function to customize Flymake fringe overlay."
+"Advice function to customize Flymake fringe overlay when passing BITMAP and RECURSED."
 (if (and (symbolp bitmap)
 		 (boundp bitmap)
 		 (not recursed))
@@ -92,15 +92,17 @@ Possible values are 'left' and 'right'."
                    'face `(:inherit (success default) :underline nil))))
 
 (defcustom flymake-margin-get-margin-symbol-default " "
-  "Blank symbol to use when `flymake-get-margin-symbol returns nothing."
+  "Blank symbol to use when `flymake-margin-get-margin-symbol returns nothing."
   :type 'string
   :group 'flymake-margin)
 
-(defun flymake-get-margin-symbol (&optional beg end)
-  "Return margin symbol based on the severity of Flymake diagnostics for the specified line range.
-If BEG or END is not provided, default to the beginning or end position of the current line.
-Ideally this would be used by some sort of git-gutter package (hl-diff maybe) and other
-extensions modifying the margin in order to copy the contents."
+(defun flymake-margin-get-margin-symbol (&optional beg end)
+  "Return margin symbol based on the severity of Flymake diagnostics.
+For the specified line range, if BEG or END is not provided,
+default to the beginning or end position of the current line.
+Ideally this would be used by some sort of git-gutter
+package (hl-diff maybe) and other extensions modifying the margin
+in order to copy the contents."
   (interactive)
   (setq beg (or beg (line-beginning-position)))
   (setq end (or end (line-end-position)))
@@ -128,7 +130,6 @@ extensions modifying the margin in order to copy the contents."
         (advice-add #'flymake--fringe-overlay-spec :override #'flymake-margin-fringe-overlay-spec-advice)
         (run-hooks 'flymake-margin-mode-hook))
     (progn
-      ;; TODO: Unload symbols loaded by `flymake-margin-setup-symbols`
       (advice-remove #'flymake--fringe-overlay-spec #'flymake-margin-fringe-overlay-spec-advice)
       (run-hooks 'flymake-margin-mode-off-hook))))
 
